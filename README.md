0feccf91777fe979c9b32aecb35bdac375a3a909
Andrew Gaidus <andrew.gaidus@gmail.com>
2016-07-01 15:12:13 -0700

    adding set up script and adjacency script

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python-Modules
+These are the set of modules I've created that help me do my work more efficiently.
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='pyspat',
+      version='1.0',
+      py_modules=['adjacencies'],
+      )
